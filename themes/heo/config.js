7ef787c82c5368bb93d44824c0c1f19dfc0baaa9
cjg2772 <71551566+cjg2772@users.noreply.github.com>
2025-08-08 17:46:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'https://blog.lcyteam.me',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'LCY Music Pro',
-  HEO_HERO_TITLE_LINK: 'https://music.lcyteam.me',
+  HEO_HERO_TITLE_LINK: 'https://blog.lcyteam.me/article/music',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
