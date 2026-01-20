6e5927d4fd4a74ffc147c8f58eab542b22060d1f
Jingge Chen <71551566+cjg2772@users.noreply.github.com>
2026-01-20 13:02:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,9 +16,9 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LCY Team', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Share Every Moment', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.lcyteam.me', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.lcyteam.tech', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'LCY Team, Blog', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://image.lcyteam.us.kg/file/c45da08882e745e9be273.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://image.lcyteam.tech/file/c45da08882e745e9be273.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
